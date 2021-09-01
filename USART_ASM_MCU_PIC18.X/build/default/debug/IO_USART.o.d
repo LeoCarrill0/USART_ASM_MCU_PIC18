4a77a0da0718c802c7940588d09961c17c02b6ac
LeoCarrill0 <89870928+LeoCarrill0@users.noreply.github.com>
2021-09-01 11:15:25 -0600

    27

---
+++ USART_ASM_MCU_PIC18.X/build/default/debug/IO_USART.o.d
@@ -0,0 +1,4 @@
+build/default/debug/IO_USART.o:  \
+	F:/Electronics/ASM/PIC16877A/USART.X/IO_USART.asm  \
+	F:/Electronics/ASM/PIC16877A/USART.X/Retardo.inc  \
+
---
+++ USART_ASM_MCU_PIC18.X/build/default/production/IO_USART.o.d
@@ -0,0 +1,4 @@
+build/default/production/IO_USART.o:  \
+	F:/Electronics/ASM/PIC16877A/USART.X/IO_USART.asm  \
+	F:/Electronics/ASM/PIC16877A/USART.X/Retardo.inc  \
+
